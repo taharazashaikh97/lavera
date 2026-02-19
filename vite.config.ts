8c25a4fd9a04dd90f78e009216e827a89875607b
taharazashaikh97 <taharazashaikh97@gmail.com>
2026-02-19 19:01:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  base: '/lavera/',   // 👈 your repo name
+})
